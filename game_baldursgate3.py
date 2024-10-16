57b2594a82154aa656d5dafaf152cb2283ef1cf2
zino <115655149+Alvadus@users.noreply.github.com>
2024-10-16 17:42:15 +0100

    Update game_baldursgate3.py

--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -4,12 +4,47 @@ from pathlib import Path
 import mobase
 from PyQt6.QtCore import QDir, QFileInfo, QDirIterator, QFile, QFileInfo, qDebug
 
-from ..basic_features import BasicGameSaveGameInfo, BasicLocalSavegames, BasicModDataChecker
+from ..basic_features import BasicGameSaveGameInfo, BasicModDataChecker, GlobPatterns
+from ..basic_features.basic_save_game_info import BasicGameSaveGame
 from ..basic_game import BasicGame
 
-class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper, ABC):
+
+class BG3ModDataChecker(BasicModDataChecker):
+    def __init__(self):
+        super().__init__(
+            GlobPatterns(
+                valid=[
+                    "PAK_FILES",
+                    "SE_CONFIG",
+                    "Generated",
+                    "Root",
+                ],
+                delete=[
+                    "info.json",
+                    "README",
+                    "*.Ink",
+                    "*.url",
+                ],
+                move={
+                    # PAK Mods
+                    "*.pak": "PAK_FILES/",
+                    # "*": "PAK_FILES/",
+                    # Script Extender Config Mods
+                    "*.json": "SE_CONFIG/",
+                    "BG3MCM": "SE_CONFIG/",
+                    # DLL Mods
+                    "bin": "Root/bin",
+                    "*.dll": "Root/bin/",
+                }
+            )
+        )
+
+class BG3Game(BasicGame):
     Name = "Baldur's Gate 3 Unofficial Support Plugin"
     Author = "Dragozino"
+
+    def version(self):
+        return mobase.VersionInfo(0, 0, 1, mobase.ReleaseType.PRE_ALPHA)
 
     GameName = "Baldur's Gate 3"
     GameShortName = "baldursgate3"
@@ -17,7 +52,7 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper, ABC):
     GameValidShortNames = ["baldursgate3"]
 
     GameBinary = r"bin\bg3.exe"
-    GameDataPath = r"%LOCALAPPDATA%\\Larian Studios\\Baldur's Gate 3\\Mods"
+    GameDataPath = r"Data"
     GameSavesDirectory = r"%LOCALAPPDATA%\\Larian Studios\\Baldur's Gate 3\\PlayerProfiles\\Public\\Savegames\\Story\\"
     GameSaveExtension = "lsv"
 
@@ -25,6 +60,11 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper, ABC):
     GameSteamId = 1086940
     GameGogId = 1456460669
 
-    def __init__(self):
-        BasicGame.__init__(self)
-        mobase.IPluginFileMapper.__init__(self)
+    def init(self, organizer: mobase.IOrganizer):
+        super().init(organizer)
+        self._register_feature(BG3ModDataChecker())
+        self._register_feature(BasicGameSaveGameInfo(lambda s: s.with_suffix(".png")))
+        return True
+
+    def iniFiles(self):
+        return ["modsettings.lsx"]
