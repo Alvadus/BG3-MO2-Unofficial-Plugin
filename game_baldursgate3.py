9c76ad8a9f3354ba1f8e46cb06130ae89b0cfc2a
zino <115655149+Alvadus@users.noreply.github.com>
2024-10-17 15:00:06 +0100

    Update game_baldursgate3.py

--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -2,12 +2,11 @@ from abc import ABC
 from pathlib import Path
 
 import mobase
-from PyQt6.QtCore import QDir, QFileInfo, QDirIterator, QFile, QFileInfo, qDebug
+from PyQt6.QtCore import QDir, QFileInfo, QDirIterator, QFile, qDebug
 
 from ..basic_features import BasicGameSaveGameInfo, BasicModDataChecker, GlobPatterns
 from ..basic_features.basic_save_game_info import BasicGameSaveGame
 from ..basic_game import BasicGame
-
 
 class BG3ModDataChecker(BasicModDataChecker):
     def __init__(self):
@@ -39,6 +38,11 @@ class BG3ModDataChecker(BasicModDataChecker):
             )
         )
 
+class BG3GamePlugin(mobase.GamePlugins, ABC):
+    def __init__(self, organizer: mobase.IOrganizer):
+        super().__init__()
+        self._organizer = organizer
+
 class BG3Game(BasicGame):
     Name = "Baldur's Gate 3 Unofficial Support Plugin"
     Author = "Dragozino"
@@ -63,8 +67,27 @@ class BG3Game(BasicGame):
     def init(self, organizer: mobase.IOrganizer):
         super().init(organizer)
         self._register_feature(BG3ModDataChecker())
-        self._register_feature(BasicGameSaveGameInfo(lambda s: s.with_suffix(".png")))
+        self._register_feature(BasicGameSaveGameInfo(lambda s: s.with_suffix(".webp")))
         return True
 
     def iniFiles(self):
         return ["modsettings.lsx"]
+
+    def mappings(self) -> list[mobase.Mapping]:
+        mods_path = Path(self._organizer.modsPath())
+
+        pak_mods = self.get_mods_from_type("PAK_FILES")
+        se_mods = self.get_mods_from_type("SE_CONFIG")
+
+        return []
+
+    def get_mods_from_type(self, mod_type: str):
+        mods_path = Path(self._organizer.modsPath())
+        all_mods = self._organizer.modList().allModsByProfilePriority()
+
+        mods: list[str] = []
+        for modName in all_mods:
+            if self._organizer.modList().state(modName) & mobase.ModState.ACTIVE != 0:
+                if mods_path.joinpath(modName, mod_type).exists():
+                    mods.append(modName)
+        return mods
