189813dc04361797d0db947918c8c3190c8e6990
zino <115655149+Alvadus@users.noreply.github.com>
2024-10-20 15:16:41 +0100

    Update modSettings.py

---
+++ baldursgate3/modSettings.py
@@ -0,0 +1,20 @@
+import os
+import json
+import subprocess
+import shutil
+from pathlib import Path
+from xml.dom import minidom
+from xml.etree.ElementTree import ElementTree
+
+import mobase
+from PyQt6.QtCore import QDir, QFileInfo, QDirIterator, QFile, qDebug
+
+def generate_mod_settings(modlist: mobase.IModList, profile: mobase.IProfile):
+    mod_settings = {}
+
+    print("hello")
+    return True
+
+def _fix_modscache(self):
+
+    return True
