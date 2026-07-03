18fa2d78c7551c35b4fdfff57c6519881d2f7787
zino <115655149+Alvadus@users.noreply.github.com>
2026-07-03 14:52:59 +0100

    Update version to 1.1.3

--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -272,7 +272,7 @@ class BG3ModDataChecker(BasicModDataChecker):
 class BG3Game(BasicGame, mobase.IPluginFileMapper):
     Name = "Baldur's Gate 3 Unofficial Support Plugin"
     Author = "Alvadus"
-    Version = "1.1.2"
+    Version = "1.1.3"
 
     GameName = "Baldur's Gate 3"
     GameShortName = "baldursgate3"
